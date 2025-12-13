36828bf93508e3b0f01bc7eb740ff7c7c618debc
sadr.nihad <nihadks-apa201@code.edu.az>
2025-12-13 21:25:35 +0400

    Add project files.

---
+++ InjectingTask/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InjectingTask.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error() 
+        {
+            return View();
+        }
+
+        public IActionResult Blog()
+        {
+            return View();
+        }
+
+        public IActionResult Shop()
+        {
+            return View();
+        }
+
+        public IActionResult SinglePage()
+        {
+            return View();
+        }
+
+        public IActionResult Wishlist()
+        {
+            return View();
+        }
+    }
+}
---
+++ InjectingTask/Program.cs
@@ -0,0 +1,22 @@
+namespace InjectingTask
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            var app = builder.Build();
+
+            app.UseStaticFiles();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=home}/{action=blog}"
+            );
+
+
+            app.Run();
+        }
+    }
+}
